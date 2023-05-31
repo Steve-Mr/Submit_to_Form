9d85d60b351bfaa7c7f640c43ee3cb43486a1b13
Maary <24504742+Steve-Mr@users.noreply.github.com>
2023-05-31 09:29:42 +0800

    add themed icon and monet color support

--- app/src/main/java/com/maary/logger/TransactionApplication.kt
+++ app/src/main/java/com/maary/logger/TransactionApplication.kt
@@ -1,6 +1,7 @@
 package com.maary.logger
 
 import android.app.Application
+import com.google.android.material.color.DynamicColors
 import com.maary.logger.database.TransactionRepository
 import com.maary.logger.database.TransactionRoomDatabase
 import kotlinx.coroutines.CoroutineScope
@@ -8,8 +9,13 @@ import kotlinx.coroutines.SupervisorJob
 
 class TransactionApplication: Application() {
 
-    val applicationScope = CoroutineScope(SupervisorJob())
+    private val applicationScope = CoroutineScope(SupervisorJob())
 
     val database by lazy { TransactionRoomDatabase.getDatabase(this, applicationScope) }
     val repository by lazy { TransactionRepository(database.transactionDao()) }
+
+    override fun onCreate() {
+        super.onCreate()
+        DynamicColors.applyToActivitiesIfAvailable(this)
+    }
 }
